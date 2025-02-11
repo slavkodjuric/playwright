aa5459b41dbf33aa4d5961f6a8e2cacff7ef99fb
slavkodjuric <slavkodjuric62@gmail.com>
2025-02-11 19:32:31 +0100

    added and fixed negative testcases fixed import section

--- tests/API/loginAPI.spec.js
+++ tests/API/loginAPI.spec.js
@@ -1,20 +1,55 @@
 import { test, expect } from "@playwright/test";
 import { LoginAPI } from "../../POM/modules/API/loginAPI";
 import {
-  HEADINGS,
-  URLS,
-  utils,
   VALID_LOGIN_PAYLOAD,
-  generateUserCredentials,
+  utils,
+  generateUserCredentials
 } from "../../fixtures";
 
 test.describe("login API tests", () => {
   let loginAPI;
+  const emailErrorMessage = "The email field is required";
+  const passwordErrorMessage = "The password field is required";
+  const unauthorizedResponse = "Unauthorized";
 
   test.beforeEach("instantiate class", ({ page }) => {
     loginAPI = new LoginAPI(page);
   });
-  test("login via BackEnd", async ({ page }) => {
+
+
+  test("Login with no email", async ({}) => {
+    const response = await loginAPI.login(
+      VALID_LOGIN_PAYLOAD[undefined],
+      VALID_LOGIN_PAYLOAD["PASSWORD"]
+    );
+    expect(response.message).toBe(emailErrorMessage);
+  });
+
+  test("Login with no password", async ({}) => {
+    const response = await loginAPI.login(
+      VALID_LOGIN_PAYLOAD["EMAIL"],
+      VALID_LOGIN_PAYLOAD[undefined]
+    );
+    expect(response.status).toBe(passwordErrorMessage);
+  });
+
+  test("Login with invalid password", async ({}) => {
+    const response = await loginAPI.login(
+      VALID_LOGIN_PAYLOAD["EMAIL"],
+      utils.generateRandomString(6)
+    );
+    expect(response.message).toBe(unauthorizedResponse);
+  });
+
+  test("Login with invalid email", async ({}) => {
+    const response = await loginAPI.login(
+      utils.generateRandomString(6),
+      VALID_LOGIN_PAYLOAD["PASSWORD"]
+    );
+    expect(response.message).toBe(unauthorizedResponse);
+  });
+
+  test("login via BackEnd", async ({}) => {
     const response = await loginAPI.login(
       VALID_LOGIN_PAYLOAD["EMAIL"],
       VALID_LOGIN_PAYLOAD["PASSWORD"]
@@ -24,19 +59,4 @@ test.describe("login API tests", () => {
     expect(response.user.email).toBe(VALID_LOGIN_PAYLOAD["EMAIL"]);
   });
 
-  test("Login with no email", async ({ page }) => {
-    const response = await loginAPI.login(
-      VALID_LOGIN_PAYLOAD[""],
-      VALID_LOGIN_PAYLOAD["PASSWORD"]
-    );
-    expect(response.status).toBe("The email field is required");
-  });
-
-  test("Login with no password", async ({ page }) => {
-    const response = await loginAPI.login(
-      VALID_LOGIN_PAYLOAD["EMAIL"],
-      VALID_LOGIN_PAYLOAD[""]
-    );
-    expect(response.status).toBe("The password field is required");
-  });
 });
