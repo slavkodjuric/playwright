8a4941a284a3b8e7ca55d29a9373e774c6efb846
slavkodjuric <slavkodjuric62@gmail.com>
2025-02-11 19:44:36 +0100

    added one more negative TC fixed import section

--- tests/API/registerBE.spec.js
+++ tests/API/registerBE.spec.js
@@ -1,9 +1,6 @@
 import { test, expect } from "@playwright/test";
 import {
-  HEADINGS,
-  URLS,
   utils,
-  VALID_LOGIN_PAYLOAD,
   generateUserCredentials,
 } from "../../fixtures";
 import { RegisterAPI } from "../../POM/modules/API/registerAPI";
@@ -11,30 +8,37 @@ import { RegisterAPI } from "../../POM/modules/API/registerAPI";
 test.describe("register API backend", () => {
   let registerAPI;
   const { username, email, password } = generateUserCredentials(6);
+  const emailInvalidResponse = "The email field format is invalid.";
 
   test.beforeEach("register user", ({ page }) => {
     registerAPI = new RegisterAPI(page);
   });
 
-  test("register a user no username", async ({ page }) => {
+  test("register a user no username", async ({}) => {
     const response = await registerAPI.register(email, password);
 
     expect(response.status).toBe("The username field is required");
   });
 
-  test("register user without email", async ({ page }) => {
+  test("register user without email", async ({}) => {
     const response = await registerAPI.register(username, password);
 
     expect(response.status).toBe("The email field is required");
   });
 
-  test("register user without password", async ({ page }) => {
+  test("register user without password", async ({}) => {
     const response = await registerAPI.register(username, email);
 
     expect(response.status).toBe("The password field is required");
   });
 
-  test("register a user using backend", async ({ page }) => {
+  test("register user with invalid email format", async({})=>{
+    const response = await registerAPI.register(username, utils.generateRandomString(6), password);
+
+    expect(response.message).toBe(emailInvalidResponse);
+  })
+
+  test("register a user using backend", async ({}) => {
     const response = await registerAPI.register(username, email, password);
 
     expect(response.status).toBe("Success");
