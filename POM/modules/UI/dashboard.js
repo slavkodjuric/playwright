d6c0ac18d1465d59c285dee6ed89be5a3cac2eec
slavkodjuric <slavkodjuric62@gmail.com>
2025-02-13 17:06:51 +0100

    added cart notification locator

--- POM/modules/UI/dashboard.js
+++ POM/modules/UI/dashboard.js
@@ -1,9 +1,10 @@
-export class dashboardCart {
+export class DashboardCart {
     constructor(page) {
         this.page = page;
-        this.productCartButton = page.locator(".p-tooltip-target-wrapper");
+        this.addProductCartButton = page.locator(".p-tooltip-target-wrapper");
         this.pageCartButton = page.locator(".inline-flex items-center py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-primary hover:text-gray-600 focus:outline-none transition ease-in-out duration-150 p-button p-component");
         this.removeFromCartButton = page.locator(".p-button-icon p-c pi pi-times");
+        this.productAddedNotification = page.locator(".Toastify__toast-icon Toastify--animate-icon Toastify__zoom-enter");
     }
 
     async cart(){
